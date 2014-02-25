70a77627ee19bb4569d8d105177a19e3d67b0142
Ken Mercus Lai <ken.mercus.lai@gmail.com>
2014-02-26 00:10:05 +0800

    fixed img size issue in blog post list.

--- static/js/application.js
+++ static/js/application.js
@@ -34,27 +34,24 @@
             }
             
         };
-    };
+    }
 
-    // ajax load blog index
+    // ajax-like load blog index
     var counter = makeCounter();
     var index_num = counter(0);
     var POST_LIMIT = 3;
     var POST_COUNT = parseInt($('#blog_main_area').attr('count'), 10);
-    // alert(POST_COUNT);
     $('#prev').on('click', function (e) {
       index_num -= 1;
       if (index_num === 0 ) {
         index_num = 1;
-        // alert(index_num);
         $(this).addClass('disabled');
       } else {
-        // alert(index_num);
         $('#next').removeClass('disabled');
         $('#blog_main_area div.item').each( function(){
           if ($(this).is(':visible')) {
             $(this).hide('slow');
-          };
+          }
         });
       
         $('#blog_main_area div.item').slice((index_num-1)*POST_LIMIT, (index_num)*POST_LIMIT) .each( function(){
@@ -62,7 +59,6 @@
         }); 
 
         if (index_num === 1) {
-          // alert(index_num);
           $(this).addClass('disabled');
         }     
       }
@@ -70,15 +66,13 @@
     });
     $('#next').on('click', function (e) {
       index_num += 1;
-      // alert(index_num);
       $('#prev').removeClass('disabled');
 
       if (index_num * POST_LIMIT <= POST_COUNT) {
-        // alert(index_num * POST_LIMIT);
         $('#blog_main_area div.item').each( function(){
           if ($(this).is(':visible')) {
             $(this).hide('slow');
-          };
+          }
         });
 
         $('#blog_main_area div.item').slice((index_num-1)*POST_LIMIT, (index_num)*POST_LIMIT) .each( function(){
@@ -89,14 +83,19 @@
       if (index_num * POST_LIMIT >= POST_COUNT ) {
         $(this).addClass('disabled');
         index_num = ~~(POST_COUNT / POST_LIMIT);
-      };
-      // alert(index_num);
+      }
     });
 
     // Activate zoom on content images in the main column and add an icon overlay (but ignore icons)
-    $("article.content img").each(function(){
+    if ($('#blog_main_area').width()) {
+      var containter_width = $('#blog_main_area').width();  
+    } else {
+      var containter_width = $('article.content').width();
+    }
+    
+    $("section#blog img").each(function(){
       // Until we properly generate thumbnails and their links on Pelican's side, we just link an image to itself.
-      if ($(this).width() > $('article.content').width()) {
+      if (this.naturalWidth > containter_width) {
         if ($(this).parents('a').length === 0) {
           $(this).wrap(
             $('<a/>').attr('href', $(this).attr('src'))
@@ -116,12 +115,13 @@
             easing: 'ease-in-out',
           },
         });
-        $(this).css('max-width', $('article.content').width());
+        $(this).css('max-width', containter_width);
         
         // // Add overlay zoom icon
         // $(this).mglass({opacity: 1,});
-      };
+      }
     });
+    
 
   });
 }(window.jQuery);
